0dc901c0c45c8de48216b178abcaaf7172c99c59
lotus <daniel.younghwan.lee@gmail.com>
2022-05-23 11:45:40 -0400

    set up api fetch & local storage test

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 5/21 meeting:
 - settled on our project idea
 - sucessfully established our GitHub workflow by creating remote repository to local repos and testing pushes and pulls
+
+5/22 meeting:
+- settled on our CSS framework, Bulma
+- started using Discord to host group meetings
+- began building HTML skeleton, including header & form
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,31 @@
 console.log("Hello world!")
-
 console.log("Hello birds!")
-
 console.log("Goodnight yall!")
 
+var inputFormEl = document.querySelector("#input-form");
+var formModalEl = document.querySelector("#form-modal");
+
+var countryStorage = [];
+
+// test local storage
+
 // test api server fetch
+
+getCovidInfo = function (countryName) {
+    var apiUrl = "https://disease.sh/v3/covid-19/countries/" + countryName;
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+            });
+        }
+    });
+};
+
+var displayCovidInfo = function (data) {
+
+};
+
+// getCovidInfo();
+
+inputFormEl.addEventListener("submit", formSubmitHandler);
